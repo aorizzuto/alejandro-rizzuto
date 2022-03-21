4c4b9b9fbf22b210da92459d15e1a68758c5ea92
Alejandro Rizzuto <alejandro@Alejandros-MacBook-Air.local>
2022-03-21 09:25:34 -0300

    Adding missing parenthesis in tests

--- src/test/java/com/xcale/challenge/validation/ValidationTests.java
+++ src/test/java/com/xcale/challenge/validation/ValidationTests.java
@@ -10,25 +10,25 @@ public class ValidationTests {
 
     @Test
     public void WHEN_validate_ID_THEN_success() throws Exception {
-        GroupRequestDTO request = new GroupRequestDTO("1", "Message", new ArrayList<>();
+        GroupRequestDTO request = new GroupRequestDTO("1", "Message", new ArrayList<>());
         Assertions.assertDoesNotThrow(() -> GroupValidation.validate(request));
     }
 
     @Test
     public void WHEN_validate_group_ID_THEN_fail() throws Exception {
-        GroupRequestDTO request = new GroupRequestDTO(null, "Message", new ArrayList<>();
+        GroupRequestDTO request = new GroupRequestDTO(null, "Message", new ArrayList<>());
         Assertions.assertThrows(Exception.class, () -> GroupValidation.validate(request));
     }
 
     @Test
     public void WHEN_validate_message_THEN_fail() throws Exception {
-        GroupRequestDTO request = new GroupRequestDTO("1", null, new ArrayList<>();
+        GroupRequestDTO request = new GroupRequestDTO("1", null, new ArrayList<>());
         Assertions.assertThrows(Exception.class, () -> GroupValidation.validate(request));
     }
 
     @Test
     public void WHEN_validate_ID_THEN_fail() throws Exception {
-        GroupRequestDTO request = new GroupRequestDTO("1", null, new ArrayList<>();
+        GroupRequestDTO request = new GroupRequestDTO("1", null, new ArrayList<>());
         Assertions.assertThrows(Exception.class, () -> IdValidation.validateId("1"));
     }
 }
